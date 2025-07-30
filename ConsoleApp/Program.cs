85594c2b9dac00fa38deb570a8f88d4cf30fb9c4
Samuel-Kim787 <joven.rakimkrishna@ue.edu.ph>
2025-07-30 01:34:08 +0000

    Yes

---
+++ ConsoleApp/Program.cs
@@ -0,0 +1,4 @@
+﻿// See https://aka.ms/new-console-template for more information
+Console.WriteLine("Name: Rakim Krishna F. Joven");
+Console.WriteLine("Student No: 20241139140");
+Console.WriteLine("It was not very difficult.");
