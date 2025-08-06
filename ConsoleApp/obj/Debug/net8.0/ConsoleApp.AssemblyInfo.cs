1f6daae3ae0b79fd8d1e8775d58e2167399270eb
Samuel-Kim787 <joven.rakimkrishna@ue.edu.ph>
2025-08-06 01:48:16 +0000

    student information

--- ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
+++ ConsoleApp/obj/Debug/net8.0/ConsoleApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+85594c2b9dac00fa38deb570a8f88d4cf30fb9c4")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d16beaad12b1957c527267231cfeea10e2c971a5")]
 [assembly: System.Reflection.AssemblyProductAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ConsoleApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
---
+++ OOPConsoleStudent/Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+
+namespace OOPConsoleStudent
+{
+    abstract class StudentInformation
+    {
+        protected string studentName;
+        protected int studentGrade;
+        
+        public void SetData(string studentName, int studentGrade)
+        {
+            this.studentName = studentName;
+            this.studentGrade = studentGrade;
+        }
+
+        public abstract void DisplayInfo();
+        public abstract void CheckStatus();
+    }
+
+    class CollegeStudent : StudentInformation
+    {
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("{ College Student }");
+            Console.WriteLine($"Student Name: {studentName}");
+            Console.WriteLine($"Student Grade: {studentGrade}");
+        }
+
+        public override void CheckStatus()
+        {
+            if (studentGrade > 3)
+            {
+                Console.WriteLine("Status: FAILED");
+            }
+            else
+            {
+                Console.WriteLine("Status: PASSED");
+            }
+        }
+    }
+
+    class SeniorStudent : StudentInformation
+    {
+        public override void DisplayInfo()
+        {
+            Console.WriteLine("{ Senior Student }");
+            Console.WriteLine($"Student Name: {studentName}");
+            Console.WriteLine($"Student Grade: {studentGrade}");
+        }
+
+        public override void CheckStatus()
+        {
+            if (studentGrade < 75)
+            {
+                Console.WriteLine("Status: FAILED");
+            }
+            else
+            {
+                Console.WriteLine("Status: PASSED");
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StudentInformation student1 = new CollegeStudent();
+            student1.SetData("Rakim", 2);
+            student1.DisplayInfo();
+            student1.CheckStatus();
+
+            Console.WriteLine(" "); // spacing lang
+
+            StudentInformation student2 = new CollegeStudent();
+            student2.SetData("Krishna", 4);
+            student2.DisplayInfo();
+            student2.CheckStatus();
+
+            Console.WriteLine("============================================="); // spacing lang
+
+            StudentInformation student3 = new SeniorStudent();
+            student3.SetData("Flaviano", 85);
+            student3.DisplayInfo();
+            student3.CheckStatus();
+
+            Console.WriteLine(" "); // spacing lang
+
+            StudentInformation student4 = new SeniorStudent();
+            student4.SetData("Joven", 74);
+            student4.DisplayInfo();
+            student4.CheckStatus();
+        }
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
